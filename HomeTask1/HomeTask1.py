89ba9b02630f9e49ed7a34f68d10359e2342c35b
Oleksandr_Balkashyn <Oleksandr_Balkashyn@epam.com>
2021-03-01 17:47:23 +0200

    Fixed all comments

--- HomeTask1/HomeTask1.py
+++ HomeTask1/HomeTask1.py
@@ -1,23 +1,37 @@
 import sys
+from string import ascii_lowercase
 
 
 # Task #1. Fizz Buzz
-def fizz_buzz():
-    for number in range(1, 100):
-        if number % 3 == 0 and number % 5 != 0:
-            print(f"{number} = Fizz")
-        elif number % 3 != 0 and number % 5 == 0:
-            print(f"{number} = Buzz")
-        elif number % 3 == 0 and number % 5 == 0:
-            print(f"{number} = FizzBuzz")
+# def fizz_buzz():
+#     for number in range(1, 100):
+#         if number % 3 == 0 and number % 5 != 0:
+#             print("Fizz")
+#         elif number % 3 != 0 and number % 5 == 0:
+#             print("Buzz")
+#         elif number % 3 == 0 and number % 5 == 0:
+#             print("FizzBuzz")
+#         else:
+#             print(number)
 
 
 # Task #2. Find min and max
 def min_and_max():
     numbers = [1, 2, '0', '300', -2.5, 'Dog', True, 0o1256, None]
-    min_value = min(value for value in numbers if isinstance(value, (int, float)))
-    max_value = max(value for value in numbers if isinstance(value, (int, float)))
-
+    min_value = numbers[0]
+    max_value = numbers[0]
+    for number in numbers:
+        try:
+            value = int(number)
+            if value > max_value:
+                max_value = value
+            if value < min_value:
+                min_value = value
+            print(int(number))
+        except TypeError:
+            pass
+        except ValueError:
+            pass
     print(f"Min value is = {min_value}")
     print(f"Max value is = {max_value}")
 
@@ -28,37 +42,28 @@ def letters_count():
     accuracy = 0
     with open('story', 'r', encoding='UTF8') as story:
         for line in story:
-            formatted_line = format_line(line)
-            for word in formatted_line.split(' '):
-                if word == 'python':  # 'python' in word: depends on what is the question - separate word or included.
-                    accuracy += 1
-                count_letters(dictionary, word)
+            formatted_line = line.lower()
+            accuracy += formatted_line.count('python')
+            count_letters(dictionary, formatted_line)
 
     print(f"Word Python (ignore case) was presented {accuracy} time(s).")
-    print(f"letter frequency {dictionary}")
+    most_freq_letter = max(dictionary, key=dictionary.get)
+    print(f"Most frequently used letter is {most_freq_letter} - {dictionary[most_freq_letter]} time(s).")
 
 
-def count_letters(dictionary, word):
-    for letter in word:
-        if letter in dictionary and not letter.isdigit():
-            dictionary[letter] += 1
-        elif not letter.isdigit():
-            dictionary[letter] = 1
-
-
-def format_line(line):
-    separators = ['\n', ',', '.', '-', "'", '!']
-    line = line.lower()
-    for separator in separators:
-        line = line.replace(separator, '')
-    return line
+def count_letters(dictionary, line):
+    for letter in ascii_lowercase:
+        if letter not in dictionary:
+            dictionary[letter] = line.count(letter)
+        else:
+            dictionary[letter] += line.count(letter)
 
 
 # Task #4. Find sum of multipliers 3 and 5
 def multiples_3_5_sum(n):
-    threes = 3 * ((n - 1) // 3 + 1) * ((n - 1) // 3) / 2
-    fives = 5 * ((n - 1) // 5 + 1) * ((n - 1) // 5) / 2
-    fifteens = 15 * ((n - 1) // 15 + 1) * ((n - 1) // 15) / 2
+    threes = 3 * (n // 3 + 1) * (n // 3) / 2
+    fives = 5 * (n // 5 + 1) * (n // 5) / 2
+    fifteens = 15 * (n // 15 + 1) * (n // 15) / 2
 
     result = threes + fives - fifteens
     return int(result)
@@ -66,11 +71,11 @@ def multiples_3_5_sum(n):
 
 def loops(number):
     result = 0
-    for value in range(3, number, 3):
+    for value in range(3, number + 1, 3):
         result += value
-    for value in range(5, number, 5):
+    for value in range(5, number + 1, 5):
         result += value
-    for value in range(15, number, 15):
+    for value in range(15, number + 1, 15):
         result -= value
     return result
 
@@ -81,9 +86,9 @@ def file_size(size):
     counter = 0
     label = 'B'
     while size > power:
-        size /= power
-        if counter == 4:
+        if counter == 3:
             break
+        size /= power
         counter += 1
     if counter == 1:
         label = "Kb"
@@ -92,7 +97,7 @@ def file_size(size):
     elif counter >= 3:
         label = "Gb"
 
-    return "{:.1f}{}".format(float(size), label)
+    return f"{float(size):.1f}{label}"
 
 
 def line_separator():
@@ -100,7 +105,7 @@ def line_separator():
 
 
 if __name__ == '__main__':
-    fizz_buzz()
+    # fizz_buzz()
     line_separator()
     min_and_max()
     line_separator()
@@ -120,6 +125,6 @@ if __name__ == '__main__':
     print(file_size(12345))
     print(file_size(1101947))
     print(file_size(572090))
-    print(file_size(999999999999))
+    print(file_size(9223372036854776000))
     line_separator()
     print(file_size(sys.maxsize))
